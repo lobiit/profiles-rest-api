86eb38189b6ae69182ef87dd72cfeda61d8005c5
Tumbo Lobi John <johnlobiit@gmail.com>
2021-08-25 11:22:52 +0300

    Added HelloApiView

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -23,6 +23,8 @@ class UserProfileManager(BaseUserManager):
     def create_superuser(self, email, name, password):
         """Create and save a new superuser with given details"""
         user = self.create_user(email, name, password)
+
+        user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
 
